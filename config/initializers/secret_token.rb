414ead07b2fd9fffe1c19a0d0a180505e3070c32
Niklas Begley <niklas.begley@gmail.com>
2014-01-27 18:39:10 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogOfAwesome::Application.config.secret_key_base = 'e5910699e01869b80822712c15a2a13ee3d3875ebfbdb35999cf87114c14f9204d304ac2ba1a3d14b7076fe4d0547b9d1a718404162493907a98fab4855bf3e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BlogOfAwesome::Application.config.session_store :cookie_store, key: '_BlogOfAwesome_session'
